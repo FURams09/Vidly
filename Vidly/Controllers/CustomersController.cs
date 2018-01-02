7018fdece16bd8d3cc9637a9c205cc962cf1bf47
Greg Padin <padin.greg@gmail.com>
2018-01-02 00:15:46 -0500

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+		// GET: Customer
+		public ActionResult Index()
+		{ 
+			var staticCustomersView = GetCustomers();
+            return View(staticCustomersView);
+        }
+
+		public ActionResult Details(int id)
+		{
+			var customer = GetCustomers().SingleOrDefault(c => c.ID == id);
+			return View(customer);
+		}
+
+		private IEnumerable<Customer> GetCustomers()
+		{
+			return new List<Customer>
+			{
+				new Customer {ID = 1, Name = "John Smith" },
+				new Customer {ID = 2, Name = "Mary Williams" }
+			};
+		}
+
+    }
+}
